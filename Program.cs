a229847e266b8713bc53653f07904714b0fc1769
Jenni <e107ti@etec.sp;gov;br>
2023-03-10 22:36:09 -0300

    Resultado

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("De noite.");
+Console.ResetColor();
+Console.WriteLine();
+Console.WriteLine("Uma árvore nua");
+Console.WriteLine("aponta o céu. Numa ponta");
+Console.WriteLine("brota um fruto. A lua?");
